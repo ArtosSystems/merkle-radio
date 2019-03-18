0cb0e7cd226f4df7a337f8b8010d7f975e895207
JulienDeray <julien@deray.fr>
2019-03-18 14:25:23 +0000

    uses JSyn

--- build.sbt
+++ build.sbt
@@ -3,3 +3,12 @@ name := "merkle-radio"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % "2.5.21",
+  "com.jsyn" % "jsyn" % "20170815",
+)
+
+resolvers ++= Seq[Resolver](
+  "Clojars" at "https://clojars.org/repo"
+)
--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,36 +1,50 @@
-import javax.sound.sampled.{AudioFormat, AudioSystem}
+import com.jsyn.{JSyn, Synthesizer}
+import com.jsyn.unitgen.LineOut
+import com.jsyn.unitgen.SawtoothOscillatorBL
 
 object Boot extends App {
-  val duration = 1000
-  var frequency = 440
+
+  // Start JSyn synthesizer.
+  val synthesizer: Synthesizer = JSyn.createSynthesizer()
+
+  // Create some unit generators.
+  val osc = new SawtoothOscillatorBL
+  val lineOut = new LineOut
+
+  synthesizer.add(osc)
+  synthesizer.add(lineOut)
+
+  // Connect oscillator to both left and right channels of output.
+  osc.output.connect(0, lineOut.input, 0)
+  osc.output.connect(0, lineOut.input, 1)
+
+  // Start the unit generators so they make sound.
+  osc.start()
+  lineOut.start()
+  synthesizer.start()
 
   val intervalGap = Math.pow(2.0, 1.0/12)
 
-  play(440, 500)
-  play(440 * Math.pow(intervalGap, 2), 500)
-  play(440 * Math.pow(intervalGap, 4), 500)
-  play(440 * Math.pow(intervalGap, 5), 500)
-  play(440 * Math.pow(intervalGap, 7), 500)
-  play(440 * Math.pow(intervalGap, 9), 500)
-  play(440 * Math.pow(intervalGap, 11), 500)
-  play(440 * Math.pow(intervalGap, 12), 500)
+  play(Note(440, 1.0))
+  play(Note(440 * Math.pow(intervalGap, 2), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 4), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 5), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 7), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 9), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 11), 1.0))
+  play(Note(440 * Math.pow(intervalGap, 12), 1.0))
 
-  def play(frequency: Double, duration: Int): Unit = {
-    val rate = 44100.toFloat
-    val buf = new Array[Byte](1)
-    val af = new AudioFormat(rate, 8, 1, true, false)
-    val sdl = AudioSystem.getSourceDataLine(af)
-    sdl.open()
-    sdl.start()
-    var i = 0
-    while (i < duration * rate / 1000) {
-      val angle = i / (rate / frequency) * 2.0 * Math.PI
-      buf(0) = (Math.sin(angle) * 100).toByte
-      sdl.write(buf, 0, 1)
+  // Stop units and delete them to reclaim their resources.
+  osc.stop()
+  lineOut.stop()
+  synthesizer.stop()
 
-      i += 1
-      i - 1
-    }
-    sdl.drain()
+  def play(note: Note): Unit = {
+    osc.frequency.set(note.frequency)
+    osc.amplitude.set(0.8)
+
+    synthesizer.sleepFor(note.duration)
   }
 }
+
+case class Note(frequency: Double, duration: Double)
