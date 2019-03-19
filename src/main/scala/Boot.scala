196abeed465da1c95ebfcd6b01d9350269965596
JulienDeray <julien@deray.fr>
2019-03-19 10:14:08 +0000

    adds merkle root source

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,10 +1,7 @@
-import BeatMaker.{Note, Up}
-import com.jsyn.{JSyn, Synthesizer}
-import com.jsyn.unitgen.LineOut
-import com.jsyn.unitgen.SawtoothOscillatorBL
+import music.{BeatMaker, Note, Up}
 
 object Boot extends App {
-  val beatMaker = init()
+  val beatMaker = BeatMaker()
 
   val tonic = Note(440, .5)
 
@@ -18,27 +15,4 @@ object Boot extends App {
   beatMaker.play(tonic.octave(Up))
 
   beatMaker.stop()
-
-  private def init() = {
-    // Start JSyn synthesizer.
-    val synthesizer: Synthesizer = JSyn.createSynthesizer()
-
-    // Create some unit generators.
-    val osc = new SawtoothOscillatorBL
-    val lineOut = new LineOut
-
-    synthesizer.add(osc)
-    synthesizer.add(lineOut)
-
-    // Connect oscillator to both left and right channels of output.
-    osc.output.connect(0, lineOut.input, 0)
-    osc.output.connect(0, lineOut.input, 1)
-
-    // Start the unit generators so they make sound.
-    osc.start()
-    lineOut.start()
-    synthesizer.start()
-
-    new BeatMaker(synthesizer, osc, lineOut: LineOut)
-  }
 }
---
+++ src/main/scala/music/BeatMaker.scala
@@ -0,0 +1,45 @@
+package music
+
+import com.jsyn.{JSyn, Synthesizer}
+import com.jsyn.unitgen.{LineOut, SawtoothOscillatorBL}
+
+class BeatMaker(synthesizer: Synthesizer, osc: SawtoothOscillatorBL, lineOut: LineOut) {
+  def play(note: Note): Unit = {
+    println(note.frequency)
+    osc.frequency.set(note.frequency)
+    osc.amplitude.set(0.8)
+
+    synthesizer.sleepFor(note.duration)
+  }
+
+  def stop(): Unit = {
+    osc.stop()
+    lineOut.stop()
+    synthesizer.stop()
+  }
+}
+
+object BeatMaker {
+  def apply(): BeatMaker = {
+    // Start JSyn synthesizer.
+    val synthesizer: Synthesizer = JSyn.createSynthesizer()
+
+    // Create some unit generators.
+    val osc = new SawtoothOscillatorBL
+    val lineOut = new LineOut
+
+    synthesizer.add(osc)
+    synthesizer.add(lineOut)
+
+    // Connect oscillator to both left and right channels of output.
+    osc.output.connect(0, lineOut.input, 0)
+    osc.output.connect(0, lineOut.input, 1)
+
+    // Start the unit generators so they make sound.
+    osc.start()
+    lineOut.start()
+    synthesizer.start()
+
+    new BeatMaker(synthesizer, osc, lineOut)
+  }
+}
